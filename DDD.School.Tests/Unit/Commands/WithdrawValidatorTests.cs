1426db3a6cae23f5f34e6927e7b1532c912fc065
Davide Guida <davide_guida@dell.com>
2019-10-03 23:02:59 +0200

    added withdraw

---
+++ DDD.School.Tests/Unit/Commands/WithdrawValidatorTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DDD.School.Commands;
+using DDD.School.Persistence;
+using FluentAssertions;
+using NSubstitute;
+using Xunit;
+
+namespace DDD.School.Tests.Unit.Commands
+{
+    public class WithdrawValidatorTests
+    {
+        [Fact]
+        public async Task ValidateAsync_should_fail_when_Student_does_not_exists()
+        {
+            var repo = NSubstitute.Substitute.For<IStudentsRepository>();
+            var unitOfWork = NSubstitute.Substitute.For<ISchoolUnitOfWork>();
+            unitOfWork.StudentsRepository.ReturnsForAnyArgs(repo);
+            var sut = new WithdrawValidator(unitOfWork);
+
+            var command = new Withdraw(Guid.NewGuid(), Guid.NewGuid());
+            var result = await sut.ValidateAsync(command, CancellationToken.None);
+            result.Should().NotBeNull();
+            result.Success.Should().BeFalse();
+            result.Errors.Should().Contain(e =>
+                e.Context == nameof(Withdraw.StudentId) && e.Message.Contains(command.StudentId.ToString()));
+        }
+
+        [Fact]
+        public async Task ValidateAsync_should_fail_when_Course_does_not_exists()
+        {
+            var unitOfWork = NSubstitute.Substitute.For<ISchoolUnitOfWork>();
+            var sut = new WithdrawValidator(unitOfWork);
+
+            var command = new Withdraw(Guid.NewGuid(), Guid.NewGuid());
+            var result = await sut.ValidateAsync(command, CancellationToken.None);
+            result.Should().NotBeNull();
+            result.Success.Should().BeFalse();
+            result.Errors.Should().Contain(e =>
+                e.Context == nameof(Withdraw.CourseId) && e.Message.Contains(command.CourseId.ToString()));
+        }
+
+        [Fact]
+        public async Task ValidateAsync_should_succeed_when_command_valid()
+        {
+            var student = new Student(Guid.NewGuid(), "existing", "Student");
+
+            var studentsRepository = NSubstitute.Substitute.For<IStudentsRepository>();
+            studentsRepository.FindByIdAsync(Arg.Any<Guid>(), Arg.Any<CancellationToken>())
+                .ReturnsForAnyArgs(student);
+
+            var course = new Course(Guid.NewGuid(), "existing course");
+
+            var coursesRepository = NSubstitute.Substitute.For<ICoursesRepository>();
+            coursesRepository.FindByIdAsync(Arg.Any<Guid>(), Arg.Any<CancellationToken>())
+                .ReturnsForAnyArgs(course);
+
+            var unitOfWork = NSubstitute.Substitute.For<ISchoolUnitOfWork>();
+            unitOfWork.StudentsRepository.ReturnsForAnyArgs(studentsRepository);
+            unitOfWork.CoursesRepository.ReturnsForAnyArgs(coursesRepository);
+            var sut = new WithdrawValidator(unitOfWork);
+
+            var command = new Withdraw(course.Id, student.Id);
+            var result = await sut.ValidateAsync(command, CancellationToken.None);
+            result.Should().NotBeNull();
+            result.Success.Should().BeTrue();
+            result.Errors.Should().BeEmpty();
+        }
+    }
+}
---
+++ DDD.School/Commands/Withdraw.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DDD.School.Persistence;
+
+namespace DDD.School.Commands
+{
+    public class Withdraw : MediatR.INotification
+    {
+        public Withdraw(Guid courseId, Guid studentId)
+        {
+            if (Guid.Empty == courseId)
+                throw new ArgumentOutOfRangeException(nameof(courseId));
+            if (Guid.Empty == studentId)
+                throw new ArgumentOutOfRangeException(nameof(studentId));
+            CourseId = courseId;
+            StudentId = studentId;
+        }
+
+        public Guid CourseId { get; }
+        public Guid StudentId { get; }
+    }
+
+    public class WithdrawValidator : BaseValidator<Withdraw>
+    {
+        private readonly ISchoolUnitOfWork _unitOfWork;
+
+        public WithdrawValidator(ISchoolUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+        }
+
+        protected override async Task RunAsync(Withdraw command, ValidationResult result, CancellationToken cancellationToken)
+        {
+            var courseById = await _unitOfWork.CoursesRepository.FindByIdAsync(command.CourseId, cancellationToken);
+            if (null == courseById)
+                result.AddError(nameof(Withdraw.CourseId), $"invalid course id {command.CourseId}");
+
+            var studentById = await _unitOfWork.StudentsRepository.FindByIdAsync(command.StudentId, cancellationToken);
+            if (null == studentById)
+                result.AddError(nameof(Withdraw.StudentId), $"invalid student id {command.StudentId}");
+        }
+    }
+
+    public class WithdrawHandler : BaseCommandHandler<Withdraw>
+    {
+        private readonly ISchoolUnitOfWork _unitOfWork;
+        public WithdrawHandler(IValidator<Withdraw> validator, ISchoolUnitOfWork unitOfWork) : base(validator)
+        {
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+        }
+
+        protected override async Task RunCommand(Withdraw command, CancellationToken cancellationToken)
+        {
+            var course = await _unitOfWork.CoursesRepository.FindByIdAsync(command.CourseId, cancellationToken); 
+            var student = await _unitOfWork.StudentsRepository.FindByIdAsync(command.StudentId, cancellationToken);
+            student.Withdraw(course);
+            await _unitOfWork.CommitAsync(cancellationToken);
+        }
+    }
+}
